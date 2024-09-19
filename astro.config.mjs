46b3ebaf7198969e128d18c289c65016adf83922
javihll <javihll@gmail.com>
2024-09-19 16:11:30 +0200

    Subida inicial

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    base: 'javihll.github.io/my-portfolio',
+    integrations: [react()]
+});
